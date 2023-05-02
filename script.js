e743a1f198c971e2b482eda5d1288046e1b578b6
Brandon Gentile <gentile.brandon@gmail.com>
2023-05-01 20:18:36 -0400

    Make basic math functions

---
+++ script.js
@@ -0,0 +1,15 @@
+const add = function (a, b) {
+  console.log(a + b);
+};
+
+const subtract = function (a, b) {
+  console.log(a - b);
+};
+
+const product = function (a, b) {
+  console.log(a * b);
+};
+
+const divide = function (a, b) {
+  console.log(a / b);
+};
